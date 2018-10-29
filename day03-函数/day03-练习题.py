ea8a8f6573bc21d0964bf67896f5dd04f65324ed
jane <782108321@qq.com>
2018-10-29 19:50:05 +0800

    update day03
    by zhangz-l

--- day03-函数/day03-练习题.py
+++ day03-函数/day03-练习题.py
@@ -69,16 +69,20 @@ PS: 字典中的value只能是字符串或列表
 写函数，此函数只接收一个参数且此参数必须是列表数据类型，此函数完成的功能是返回给调用者一个字典，此字典的键值对为此列表的索引及对应的元素。
 例如传入的列表为：[11, 22, 33]返回的字典为{0: 11, 1: 22, 2: 33}。
 '''
-l1 = [11, 22, 33]
-dic={}
-# for v in l1:
-#     l2 = l1.index(v)
-#     dic[l2] = v
-# print(dic)
-def list_z(x):
-    if x 
-    return
-list_z(l1)
+# l1 = [11, 22, 33]
+# dic={}
+# # for v in l1:
+# #     l2 = l1.index(v)
+# #     dic[l2] = v
+# # print(dic)
+#
+# def list_z(x):
+#     if type(x) == list:
+#         for v in x:
+#             x2 = x.index(v)
+#             dic[x2]=v
+#     print(dic)
+# list_z(l1)
 
 
 '''
--- day03-函数/test.py
+++ day03-函数/test.py
@@ -1,14 +1,23 @@
+# def count(s):
+#     dict = {'数字的个数：':0, '字母的个数：':0, '空格的个数：':0, '其他的个数：':0}
+#     for i in s:
+#         if i.isdigit():
+#             dict['数字的个数：'] += 1
+#         elif i.islower() or i.isupper():
+#             dict['字母的个数：'] += 1
+#         elif i.isspace():
+#             dict['空格的个数：'] += 1
+#         else:
+#             dict['其他的个数：'] += 1
+#     return dict
+# str = input('请输入内容：')
+# res = count(str)
+# print(res)
 
-# def fun1(n):
-#     for i,m in n.items():
-#         m = str(m)
-#         if len(m) > 2:
-#             n[i] = m[0:2]
-#     print(n)
-dic = {'name':'fanfan','sex':'boy'}
-# fun1(dic)
 
-for i,m in dic.items():
-    if len(m) > 2:
-        dic[i]=m[0:2]
-    print(dic)
+
+dic={'数字 ':0}
+stt='11452132'
+z1=stt.isdigit()
+dic['数字']+=
+print(dic)
