f9ba36bdede0ae5787878ede4bddaca4c3fa72e5
zhang-sgin <782108321@qq.com>
2018-10-31 17:21:53 +0800

    update day03
    by zhangz

--- day03-函数/test.py
+++ day03-函数/test.py
@@ -1,17 +1,6 @@
-# import os
-# def change_file(filename,old,new):
-#     with open(filename,encoding='utf-8') as f1,open('filename_temp.txt',encoding='utf-8',mode='w') as f2:
-#         for line in f1:
-#             f2.write=line.replace(old,new)
-# change_file('08.txt','123445','1234')
-# os.remove('filename')
-# os.rename('filename_temp.txt','filename')
-
-# import os
-# with open('08.txt', encoding='utf-8') as f1,open('08-bak.txt',encoding='utf-8',mode='w') as f2:
-#     for line in f1:
-#         new_line = line.replace('alex','SB')
-#         f2.write(new_line)
-# os.remove('08.txt')
-# os.rename('08-bak.txt','08.txt')
-
+def jiec(n):
+    if n == 0:
+        # return  1
+    sum = n * jiec(n - 1)
+    return  sum
+print(jiec(7))
