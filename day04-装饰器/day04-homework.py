a78a9a387200f97962598b675637dc45b949e01b
zhang-sgin <782108321@qq.com>
2018-11-07 17:06:29 +0800

    update day04
    by zhangz

--- day04-装饰器/day04-homework.py
+++ day04-装饰器/day04-homework.py
@@ -16,3 +16,76 @@
 	5)，注销用户是指注销用户的登录状态，使其在访问任何页面时，必须重新登录。
 	6)，退出程序为结束整个程序运行。
 '''
+
+import  os
+login_status={'username':None,'status':False}
+def login(x):
+    def inner(*args,**kwargs):
+        if login_status['status']:
+            ret = x(*args,**kwargs)
+            return ret
+        else:
+            count = 1
+            while count <4:
+                print('请先登录')
+                username=input('请输入用户名：').strip()
+                password=input('请输入密码：').strip()
+                with open('registry2.txt',encoding='utf8')as f_registry:
+                    for i in f_registry:
+                        login_line=i.strip().split(' ')
+                        # print(login_line)
+                        if username == login_line[0] and password ==login_line[1]:
+                            login_status['username'] = username
+                            login_status['status'] = password
+                            ret=x(*args,**kwargs)
+                            return ret
+                    else:
+                        print('用户名或密码错误，请重新输入，您还有{}次机会'.format(3 - count))
+                        count += 1
+    return inner
+
+
+def register():
+    register_user_name = input('请注册您的用户名：')
+    register_password = input('请注册您的用户名密码：')
+    with open('register', encoding='utf-8',mode='r+')as f_register:
+        for line in f_register:
+            user_info = line.split(' ')
+            if register_user_name in user_info:
+                print('用户已存在')
+                continue
+            else:
+                f_register.write(register_user_name + ' ' + register_password + '\n')
+                print('{}用户注册成功'.format(register_user_name))
+                break
+
+
+@login
+def wenzhang():
+    print('欢迎打开文章页面')
+
+@login
+def riji():
+    print('欢迎打开日记页面')
+
+@login
+def comment():
+    print('欢迎打开评论页面')
+
+@login
+def shoucang():
+    print('欢迎打开收藏页面')
+
+dic = {
+    1:login,
+    2:register,
+    3: wenzhang,
+    4: riji,
+    5:comment,
+    6:shoucang
+}
+
+while 1:
+    num = input("""欢迎来到博客园首页\n1:请登录\n2:请注册\n3:文章页面\n4:日记页面\n5:评论页面\n6:收藏页面\n7:注销\n8:退出程序\n请选择功能：""").strip()
+    dic[int(num)]()
+
--- day04-装饰器/test.py
+++ day04-装饰器/test.py
@@ -67,12 +67,82 @@
 # l1=['python%d期'% i for i in range(1,101)]
 # print(l1)
 
+#
+# l1=[i**2 for i in range(1,11)]
+# l2=[i**2 for i in range(1,31) if i %3 ==0]
+# # print(l1)
+# # print(l2)
+#
+# l1=['alex','taibai','wusir','ab','sb']
+# l3=[i.upper() for i in l1 if len(i)>3]
+# print(l3)
 
-l1=[i**2 for i in range(1,11)]
-l2=[i**2 for i in range(1,31) if i %3 ==0]
-# print(l1)
-# print(l2)
+with open('registry2.txt', encoding='utf8')as f_registry:
+    for i in f_registry:
+        login_line=i.strip().split()
+        print(login_line)
 
-l1=['alex','taibai','wusir','ab','sb']
-l3=[i.upper() for i in l1 if len(i)>3]
-print(l3)
+
+
+
+
+# login_line=[]
+# with open('registry2.txt', encoding='utf8')as f_registry:
+#     for i in f_registry:
+#         login_line.append(i.strip().split())
+#     for i2 in login_line:
+#             print(i2)
+
+
+
+
+#
+# login_status={'username':None,'status':False}
+# def login(x):
+#     def inner(*args,**kwargs):
+#         if login_status['status']:
+#             ret = x(*args,**kwargs)
+#             return ret
+#         else:
+#             count = 1
+#             while count <4:
+#                 print('请先登录')
+#                 username=input('请输入用户名：').strip()
+#                 password=input('请输入密码：').strip()
+#                 with open('registry.txt',encoding='utf8')as f_registry:
+#                     for i in f_registry:
+#                         login_line=i.strip().split(' ')
+#                         print(login_line)
+#                         for i2 in login_line:
+#                             print(i2)
+#                             if username == login_line[0] and password ==login_line[1]:
+#                                 login_status['username'] = username
+#                                 login_status['status'] = password
+#                                 ret=x(*args,**kwargs)
+#                                 return ret
+#                             else:
+#                                 print('用户名或密码错误，请重新输入，您还有{}次机会'.format(3 - count))
+#                                 count += 1
+#     return inner
+#
+# @login
+# def nice():
+#     print('漂亮，nice打印成功')
+#
+#
+# @login
+# def perfect():
+#     print('漂亮,perfect打印成功')
+#
+# dic = {
+#     1: nice,
+#     2: perfect,
+# }
+#
+# while 1:
+#     print('''
+#     1.nice
+#     2.perfect
+#     ''')
+#     num = input('请输入数字：').strip()
+#     dic[int(num)]()
