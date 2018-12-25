e5858e098c91e88ef75886e66e8d436e6fdfb649
zhang-sgin <Lz729802>
2018-12-25 17:28:50 +0800

    update day11
    by zhangz-l

--- day09-网络socket/homework-p/ftp-client.py
+++ day09-网络socket/homework-p/ftp-client.py
@@ -78,18 +78,11 @@ class client:
                 FTP_CLIENT.send(data)
                 post_status = obj.recv(1024)
                 print(post_status)
-                if post_status == 'ok':
-                    print('发送%s文件完成' % msg)
-                    break
-                else:
-                    print('发送%s文件失败' % msg)
-                    break
-
-            # post_status = obj.recv(1024)
-            # print(post_status)
-            # status = json.loads(post_status.decode('utf-8'))
-            # print(status)
-            # break
+                # if post_status == 'ok':
+                print('发送%s文件完成' % msg)
+                break
+                # else:
+                #     print('发送%s文件失败' % msg)
         FTP_CLIENT.close()
 
     def get_file(self):
@@ -119,6 +112,7 @@ def login(username,pwd):
                 print(index, i)
                 tmp[str(index)] = client_user.gg[i]
             C = input("请输入你的选择：")  # 根据不同的选择, 执行不同的函数
+            if not C:continue
             func = getattr(client_user, tmp[C])
             func()
     else:
@@ -130,99 +124,8 @@ def login(username,pwd):
 if __name__ == '__main__':
     obj=socket.socket() #创建客户端socket对象
     obj.connect(('127.0.0.1',8080))
-    usrname=input('请输入用户名：')
-    pwd=input('请输入密码：')
+    usrname=input('请输入用户名：')    # zz
+    pwd=input('请输入密码：')         # 123
     login(usrname,pwd)
 
-
-
-# def login():
-#     print('欢迎来到FTP系统！')
-#     count = 1
-#     while count < 4:
-#         print('请先登录')
-#         username = input('请输入用户名：').strip()
-#         password = input('请输入密码：').strip()
-#         with open('user', encoding='utf8')as f_registry:
-#             for i in f_registry:
-#                 login_line = i.strip().split('|')
-#                 if username == login_line[0] and password == login_line[1]:  # zz 123
-#                     print('用户%s登录成功！' % (username))
-#                     client_user = client(username)
-#                     while 1:
-#                         tmp = {}
-#                         for index, i in enumerate(client_user.gg, 1):
-#                             print(index, i)
-#                             tmp[str(index)] = client_user.gg[i]
-#                         C = input("请输入你的选择：")  # 根据不同的选择, 执行不同的函数
-#                         func = getattr(client_user, tmp[C])
-#                         func()
-# login()
-
-
-
-# FTP_CLIENT = socket.socket()
-# FTP_CLIENT.connect(('127.0.0.1', 8080))
-# while 1:
-#     msg = 'E:\python24\office.ISO'
-#     if msg.upper() == 'Q': break
-#
-#     msg_size = os.path.getsize(msg)     # 获得文件大小
-#     msg_name = re.findall('\w*[.]\w*', msg)     # 取出文件名
-#     msg_name = 'new' + msg_name[0]
-#     file_dict = {'file_name': msg_name, 'file_size': msg_size, 'action': "post_file"}  # 文件信息封装为字典
-#
-#     head_info = json.dumps(file_dict)  # 转换为json字符串
-#     print(head_info)
-#     print(len(head_info))
-#     head_info_len = struct.pack('i', len(head_info))  # 字符串长度打包
-#     print(head_info_len)
-#
-#     FTP_CLIENT.send(head_info_len)   # 发送head_info长度
-#     FTP_CLIENT.send(head_info.encode('utf-8'))   # 发送文件信息
-#     FTP_CLIENT.send(head_info_len)   # 发送head_info长度
-#     FTP_CLIENT.send(head_info.encode('utf-8'))   # 发送文件信息
-#
-#     with open(msg, 'rb')as f_msg:
-#         data = f_msg.read()
-#         FTP_CLIENT.send(data)
-#     print('发送%s文件完成' % msg)
-#     break
-# FTP_CLIENT.close()
-
-
-
-# cmd_client = socket.socket()
-# cmd_client.connect(('127.0.0.1', 8080))
-# while 1:
-#     msg = input('>>>')
-#     if msg.upper() == 'Q': break
-#
-#     file_dict={'action':'cmd'}
-#     head_info = json.dumps(file_dict)  # 转换为json字符串
-#     head_info_len = struct.pack('i',len(head_info),) # 字符串长度打包
-#
-#     cmd_client.send(head_info_len)   # 发送head_info长度
-#     cmd_client.send(head_info.encode('utf-8'))   # 发送文件信息
-#
-#
-#
-#     head_info = json.dumps(msg)  # 转换为json字符串
-#     head_info_len = struct.pack('i',len(head_info),)
-#
-#     cmd_client.send(msg.encode('utf-8'))
-#     head_bytes = cmd_client.recv(4)
-#
-#     total_size = struct.unpack('i', head_bytes)[0]
-#
-#     data_size = 0
-#     res = b''
-#     while data_size < total_size:
-#         data = cmd_client.recv(1024)
-#         res = res + data
-#         data_size = data_size + len(data)
-#
-#     print(res.decode('gbk'))
-#
-# cmd_client.close()
 
